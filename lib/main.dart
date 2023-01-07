380998ae708ea81ab12fc52133944338645cfb0c
Sparsh-R17 <sparsh170702@gmail.com>
2023-01-07 21:11:33 +0530

    Product Provider Added

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
 
 import 'package:google_fonts/google_fonts.dart';
 
 import '/screens/products_overview_screen.dart';
 import './screens/product_detail_screen.dart';
+import './providers/product_provider.dart';
+
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]).then(
@@ -19,21 +22,24 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'MyShop',
-      theme: ThemeData(
-        textTheme: GoogleFonts.latoTextTheme(
-          Theme.of(context).textTheme,
+    return ChangeNotifierProvider(
+      create: (context) => ProductProvider(),
+      child: MaterialApp(
+        title: 'MyShop',
+        theme: ThemeData(
+          textTheme: GoogleFonts.latoTextTheme(
+            Theme.of(context).textTheme,
+          ),
+          colorScheme: ColorScheme.fromSeed(
+            seedColor: Colors.purple,
+            secondary: Colors.deepOrange,
+          ),
         ),
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.purple,
-          secondary: Colors.deepOrange,
-        ),
+        home: const ProductsOverviewScreen(),
+        routes: {
+          ProductDetailScreen.routeName: (ctx) => const ProductDetailScreen(),
+        },
       ),
-      home: ProductsOverviewScreen(),
-      routes: {
-        ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
-      },
     );
   }
 }
--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/product_provider.dart';
 
 class ProductDetailScreen extends StatelessWidget {
   static const routeName = '/product-detail-screen';
@@ -9,9 +12,11 @@ class ProductDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final productId = ModalRoute.of(context)!.settings.arguments as String;
+    final loadedProuct = Provider.of<ProductProvider>(context, listen: false)
+        .findbyId(productId);
 
     return Scaffold(
-      appBar: AppBar(title: Text('title')),
+      appBar: AppBar(title: Text(loadedProuct.title)),
     );
   }
 }
